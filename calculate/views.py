bee71c94393894fa7dc7bb2f880e2c7521e80894
Stephen Weber <stephen.t.weber@gmail.com>
2014-01-17 09:16:56 -0800

    Error handling for non-int inputs

--- calculate/views.py
+++ calculate/views.py
@@ -2,9 +2,17 @@ from django.http import HttpResponse
 import json
 from utils import product
 
+ERROR_MSG_INVALID_INPUT = {'error': 'INVALID_INPUT', 'message': 'Invalid input provided.'}
+
 def index(request):
     values_strings = request.GET.getlist('values')
-    values_ints = map(int, values_strings)
+    try:
+        values_ints = map(int, values_strings)
+    except ValueError as e:
+        err_result = {'details': str(e) }
+        err_result.update(ERROR_MSG_INVALID_INPUT)
+        json_output = json.dumps(err_result)
+        return HttpResponse(json_output, status=400)
     vsum = sum(values_ints)
     vproduct = product(values_ints)
     json_output = json.dumps({'sum': vsum, 'product': vproduct})
